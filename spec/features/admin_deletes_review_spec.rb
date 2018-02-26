e7ccdec6550f0b2400b545c39e127fc385c1698f
ethunk <ethunk>
2018-02-25 19:52:23 -0500

    admin deletes review spec passing

--- spec/features/admin_deletes_review_spec.rb
+++ spec/features/admin_deletes_review_spec.rb
@@ -4,9 +4,9 @@ require "rails_helper"
 # [x] I want to be on the Boardgame show/details page and see a list of reviews
 # [x] I want to be able to delete any reviews I submit
 
-feature "As a user
+feature "As an admin
 I want to delete a review
-So that I can erase my regretful reviews" do
+So that I can erase my offensive reviews" do
 
   def login(user=nil)
     if user == nil
@@ -19,28 +19,16 @@ So that I can erase my regretful reviews" do
   end
 
   scenario "user deletes a review on a page" do
+    admin = FactoryBot.create(:user, role: 'admin')
     user = FactoryBot.create(:user)
     boardgame = FactoryBot.create(:boardgame, user: user)
-    review = FactoryBot.create(:review, body: 'This is a review', boardgame: boardgame, user:user)
-    login(user)
+    review = FactoryBot.create(:review, body: 'This is a review', boardgame: boardgame, user: user)
+    login(admin)
 
     click_link boardgame.name
     expect(page).to have_button('Delete')
     click_button 'Delete'
 
     expect(page).to_not have_content('This is a review')
-  end
-
-  scenario "a user cannot delete a review they didn't write" do
-    user_one = FactoryBot.create(:user)
-    user_two = FactoryBot.create(:user)
-    boardgame = FactoryBot.create(:boardgame, user: user_one)
-    review = FactoryBot.create(:review, body: 'This is a review', boardgame: boardgame, user: user_one)
-
-    login(user_two)
-
-    click_link boardgame.name
-    expect(page).to_not have_content('Delete')
-
   end
 end
