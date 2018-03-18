a073e70f3bf6a6893464d589fe669a246f316df0
ethunk <ethunk>
2018-03-18 15:59:38 -0400

    working on adding codeship

---
+++ helpful_code.rb
@@ -0,0 +1,5 @@
+
+# Run assets precompile locally loading env variables from a .env file
+# Running the code below will load the assests variables needed to precompile locally 
+# env $(cat .env | xargs) RAILS_ENV=production rake assets:precompile
+#
